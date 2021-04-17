a4f36583d8e71209cf3cebd4788cb85184a73e76
tyjacks <tylerjaksn@gmail.com>
2021-04-17 06:52:06 -0400

    initial commit for unit-testing framework

---
+++ unit-testing.lisp
@@ -0,0 +1,10 @@
+;;;; Unit testing framework
+;;;; From Practical Common Lisp
+
+(defun test-+ ()
+  (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
+  (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
+  (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
