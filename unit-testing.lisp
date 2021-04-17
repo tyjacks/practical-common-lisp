7628374e6b1a2d6ec932d25d3f22e121fff7ef23
tyjacks <tylerjaksn@gmail.com>
2021-04-17 07:11:45 -0400

    remove duplication and reorder since order matters for macros

--- unit-testing.lisp
+++ unit-testing.lisp
@@ -1,12 +1,13 @@
 ;;;; Unit testing framework
 ;;;; From Practical Common Lisp
 
-
-(defun test-+ ()
-  (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
-  (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
-  (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+(defmacro check (form)
+  `(report-result ,form ',form))
 
 (defun report-result (result form)
   (format t "~:[FAIL~;pass~] ... ~a~%" result form))
 
+(defun test-+ ()
+  (check (= (+ 1 2) 3))
+  (check (= (+ 1 2 3) 6))
+  (check (= (+ -1 -3) -4)))
