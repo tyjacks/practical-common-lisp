19946d5a8b7a974c1d7821745038834616861d82
tyjacks <tylerjaksn@gmail.com>
2021-04-17 08:01:42 -0400

    add a name for each test for improved reporting

--- unit-testing.lisp
+++ unit-testing.lisp
@@ -1,6 +1,8 @@
 ;;;; Unit testing framework
 ;;;; From Practical Common Lisp
 
+(defvar *test-name* nil)
+
 (defmacro with-gensyms ((&rest names) &body body)
   "Bind each var name passed in to a unique name."
   `(let ,(loop for n in names collect `(,n (gensym)))
@@ -17,19 +19,21 @@
      ,@(loop for f in forms collect `(report-result ,f ',f))))
 
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
   result)
 
 (defun test-+ ()
-  (check 
-    (= (+ 1 2) 3)
-    (= (+ 1 2 3) 6)
-    (= (+ -1 -3) -4)))
+  (let ((*test-name* 'test-+)) 
+    (check 
+      (= (+ 1 2) 3)
+      (= (+ 1 2 3) 6)
+      (= (+ -1 -3) -4))))
 
 (defun test-* ()
-  (check
-    (= (* 2 2) 4)
-    (= (* 3 5 6) 15)))
+  (let ((*test-name* 'test-*))
+    (check
+      (= (* 2 2) 4)
+      (= (* 3 5 6) 15))))
 
 (defun test-arithmetic ()
   (combine-results
