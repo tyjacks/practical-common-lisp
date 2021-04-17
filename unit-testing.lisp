a571c57f15c39db9a603920b5eb0106f9549991f
tyjacks <tylerjaksn@gmail.com>
2021-04-17 06:56:41 -0400

    missing a parenthesis on report-result

--- unit-testing.lisp
+++ unit-testing.lisp
@@ -1,10 +1,12 @@
 ;;;; Unit testing framework
 ;;;; From Practical Common Lisp
 
+
 (defun test-+ ()
   (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
   (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
   (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
 
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+
