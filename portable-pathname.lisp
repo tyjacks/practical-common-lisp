17ca6b06a63eedbc46680a80b21c54efb7e2d10e
tyjacks <tylerjaksn@gmail.com>
2021-05-06 19:44:42 -0400

    Add functionality to walk a directory recursively

--- portable-pathname.lisp
+++ portable-pathname.lisp
@@ -84,3 +84,27 @@
 
   #-(or sbcl cmu lispworks openmcl allegro clisp)
   (error "file-exists-p not implemented"))
+
+(defun pathname-as-file (name)
+  (let ((pathname (pathname name)))
+    (when (wild-pathname-p pathname)
+      (error "Can't reliably convert wild pathnames."))
+    (if (directory-pathname-p name)
+      (let* ((directory (pathname-directory pathname))
+             (name-and-type (pathname (first (last directory)))))
+        (make-pathname
+          :directory (butlast directory)
+          :name (pathname-name name-and-type)
+          :defaults pathname))
+      pathname)))
+
+(defun walk-directory (dirname fn &key directories (test (constantly t)))
+  (labels
+    ((walk (name)
+       (cond
+         ((directory-pathname-p name)
+          (when (and directories (funcall test name))
+            (funcall fn name))
+          (dolist (x (list-directory name)) (walk x)))
+         ((funcall test name) (funcall fn name)))))
+    (walk (pathname-as-directory dirname))))
